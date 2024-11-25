c17ecc69c96528057090cfd5cf8d99579fbca9e9
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-25 14:53:15 +0530

    Test

--- DemoDI/DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
+++ DemoDI/DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoDiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDiApplication.class, args);
 	}
