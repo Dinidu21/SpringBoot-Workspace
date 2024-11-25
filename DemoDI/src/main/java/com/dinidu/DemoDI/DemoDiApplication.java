dd247b291ba3000d5ba9c088b90cfeca007d9a93
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-25 17:24:06 +0530

    initial Setup 2024-11-25 17-24-06 @Dinidu21

--- DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
+++ DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoDiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Its Working Git Hooks!!");
 		SpringApplication.run(DemoDiApplication.class, args);
 	}
 }
