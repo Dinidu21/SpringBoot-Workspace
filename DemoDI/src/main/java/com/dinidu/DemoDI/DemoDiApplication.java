9dc78522281d1ee131cd189f9a3462208df47688
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-25 17:05:34 +0530

    InteliJ 2024-11-25 17-05-34 @Dinidu21

--- DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
+++ DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoDiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Heeeey!!");
+		System.out.println("Hedeeey!!");
 		SpringApplication.run(DemoDiApplication.class, args);
 	}
-
 }
