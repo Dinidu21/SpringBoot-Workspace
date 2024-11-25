c5328e4c35a19d9435693271d819e351c9427e9d
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-26 00:07:40 +0530

    Injections in Spring Ref vars & Non Ref vars 2024-11-26 00-07-40 @Dinidu21

--- SpringLearn/src/main/java/com/dinidu/spring/Dev.java
+++ SpringLearn/src/main/java/com/dinidu/spring/Dev.java
@@ -1,12 +1,46 @@
 package com.dinidu.spring;
-
 public class Dev {
+    private Laptop laptop;
+    private int age;
+
+    /*There are 2 way to initialize the value Constructor Injection and Setter Injection*/
 
     public Dev() {
         System.out.println("Dev Object Created");
     }
 
+    //Constructor Injection for var
+    public Dev(int age) {
+        this.age = age;
+        System.out.println("Dev Object Created");
+    }
+
+    //Constructor Injection for Ref
+    public Dev(Laptop laptop) {
+        this.laptop  = laptop;
+    }
+
+    public Laptop getLaptop() {
+        return laptop;
+    }
+
+    public Dev setLaptop(Laptop laptop) {
+        this.laptop = laptop;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Dev setAge(int age) {
+        this.age = age;
+        return this;
+    }
+
     public void build() {
         System.out.println("Dev Building Project");
+        laptop.display();
+        System.out.println("Age : " + age);
     }
 }
