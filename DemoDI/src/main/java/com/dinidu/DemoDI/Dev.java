94429a9ed79b2765553e39d462f9dded1d75f5e9
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-25 22:07:10 +0530

    use @Qualifier 2024-11-25 22-07-10 @Dinidu21

--- DemoDI/src/main/java/com/dinidu/DemoDI/Dev.java
+++ DemoDI/src/main/java/com/dinidu/DemoDI/Dev.java
@@ -1,15 +1,23 @@
 package com.dinidu.DemoDI;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
 //    @Autowired //Field injection not Recommended
+    @Qualifier("laptop")
     private final Computer computer;
     //Now there is 2 options Desktop and Laptop now error occurs what to choose in here
  /* we can solve this using @Qualifier("that class name which we want in simple words") annotation
  * and another way is remove @Component annotation from the which is not required to make an object
  * and We can use @Primary annotation to make a default object
+ * Key Best Practices for @Autowired
+    Prefer Constructor Injection for mandatory dependencies.
+    Use @Qualifier or @Primary to resolve ambiguity.
+    Avoid excessive field injection; it can lead to less testable and harder-to-maintain code.
+    Break circular dependencies to prevent runtime issues.
+ *
  * */
 
     public Dev(Computer computer) { //Constructor injection
