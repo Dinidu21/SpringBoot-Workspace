6f0c11e776d65f897f209a342f9beaa23c87dc83
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-11-25 17:06:23 +0530

    Git Hooks Worked 2024-11-25 17-06-23 @Dinidu21

--- DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
+++ DemoDI/src/main/java/com/dinidu/DemoDI/DemoDiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoDiApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hedeeey!!");
+		System.out.println("Its Working Git Hooks!!");
 		SpringApplication.run(DemoDiApplication.class, args);
 	}
 }
