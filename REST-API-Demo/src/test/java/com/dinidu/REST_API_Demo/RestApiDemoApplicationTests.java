332b354cc8fb67c6f91af97318b7cac816358b31
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-12-03 23:06:59 +0530

    Added REST-API-Demo project to the repository 2024-12-03 23-06-59 @Dinidu21

---
+++ REST-API-Demo/src/test/java/com/dinidu/REST_API_Demo/RestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinidu.REST_API_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
