ea7af802aa4cb7cbe8c5973fb464dd6d0c97b412
Dinidu Sachintha <dinidusachintha3@gmail.com>
2024-12-04 18:30:08 +0530

    COMPLETED & make a JAR 2024-12-04 18-30-08 @Dinidu21

--- REST-API-Demo/src/main/java/com/dinidu/REST_API_Demo/student/StudentRepository.java
+++ REST-API-Demo/src/main/java/com/dinidu/REST_API_Demo/student/StudentRepository.java
@@ -1,7 +1,6 @@
 package com.dinidu.REST_API_Demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
